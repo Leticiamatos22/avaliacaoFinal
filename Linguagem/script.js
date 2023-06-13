a0f3bb42459a4f6b6b97c19baca2e2520d7e0cea
Leticiamatos22 <cristinadematosleticia@gmail.com>
2023-06-12 22:39:53 -0300

    nova atualização

---
+++ Linguagem/script.js
@@ -0,0 +1,37 @@
+// Crie um array de frases para digitar
+var phrases = [
+    "Evolução dos projetos realizados em HTML e CSS"
+  ];
+  
+  var typingText = document.getElementById("typing-text");
+  
+  // Função para exibir as frases com o efeito de digitação
+  function typeWriter() {
+    var i = 0;
+    var j = 0;
+    var currentPhrase = "";
+ 
+  
+    function type() {
+      if (i < phrases.length) {
+        if (!isDeleting && j <= phrases[i].length) {
+          currentPhrase = phrases[i].substring(0, j++);
+          typingText.innerHTML = currentPhrase;
+        } 
+        
+  
+        if (j > phrases[i].length) {
+          isDeleting = true;
+        }
+
+  
+        setTimeout(type, 100);
+      }
+    }
+  
+    type();
+  }
+  
+  // Chame a função para iniciar a animação de digitação
+  typeWriter();
+
