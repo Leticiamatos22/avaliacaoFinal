be598abed173256cf9d2c444823ddbeaf8136bfa
Leticiamatos22 <cristinadematosleticia@gmail.com>
2023-06-14 17:41:38 -0300

    finalizado

--- Linguagem/script.js
+++ Linguagem/script.js
@@ -35,3 +35,24 @@ var phrases = [
   // Chame a função para iniciar a animação de digitação
   typeWriter();
 
+  window.onload = function() {
+    var title = document.getElementById("title");
+  
+    // Adiciona a classe CSS para iniciar a animação
+    title.classList.add("fade-in");
+  };
+
+  
+  window.onload = function() {
+    var character = document.getElementById("character");
+    var balloon = document.getElementById("balloon");
+  
+    // Função para exibir o personagem e o balão
+    function showCharacter() {
+      character.classList.remove("hidden");
+      balloon.classList.remove("hidden");
+    }
+  
+    showCharacter(); // Exibe o personagem e o balão
+  };
+
